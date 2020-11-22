e1077c521a79c14a9dfe29573f241ada7399c6e4
Chris Keefe <crk239@nau.edu>
2020-11-21 21:19:19 -0700

    Initial project build, runs Hello World, testhat wired up, maybe Travis will work?

---
+++ R/foober.R
@@ -0,0 +1,12 @@
+#' Hello World
+#'
+#' Returns the string "Hello world"
+#'
+#' @return string
+#' @export
+#'
+#' @examples
+#' print(foober()) ## Prints "Hello world"
+foober <- function() {
+  return("Hello world")
+}
